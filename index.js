9426f5ec83fa440d119d7b09eb5c7c6f68d08c10
Nadeesha Dilruwan <eclayaz@gmail.com>
2020-02-27 18:57:04 +0530

    single member not found

--- index.js
+++ index.js
@@ -23,7 +23,13 @@ app.get("/api/members", (req, res) => {
 
 // Get single member
 app.get("/api/members/:id", (req, res) => {
-  res.json(members.filter(member => member.id === parseInt(req.params.id)));
+  const found = members.some(member => member.id === parseInt(req.params.id));
+
+  if (found) {
+    res.json(members.filter(member => member.id === parseInt(req.params.id)));
+  } else {
+    res.status(400).json({ msg: `No member with the id of ${req.params.id}` });
+  }
 });
 
 const PORT = process.env.PORT || 5000;
