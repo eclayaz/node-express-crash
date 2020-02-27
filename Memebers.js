faf4557f419f8750f7fc4e22b4ae1e4d4ec5f903
Nadeesha Dilruwan <eclayaz@gmail.com>
2020-02-27 18:25:16 +0530

    get endopint to return json

---
+++ Memebers.js
@@ -0,0 +1,24 @@
+const members = [
+  {
+    id: 1,
+    name: "member 1",
+    status: "active"
+  },
+  {
+    id: 2,
+    name: "member 2",
+    status: "active"
+  },
+  {
+    id: 3,
+    name: "member 3",
+    status: "inactive"
+  },
+  {
+    id: 4,
+    name: "member 4",
+    status: "active"
+  }
+];
+
+module.exports = members;
--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 const express = require("express");
 const path = require("path");
+const members = require("./Memebers");
 
 const app = express();
 
+// This can be get done by static folder
 // app.get("/", (req, res) => {
 //   res.sendFile(path.join(__dirname, "public", "index.html"));
 // });
 
 // Set static folder
 app.use(express.static(path.join(__dirname, "public")));
+
+// Get all members
+app.get("/api/members", (req, res) => {
+  res.json(members);
+});
 
 const PORT = process.env.PORT || 5000;
 
