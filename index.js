00bbc03c9e9414e3bb55957cebab958cdfb48167
Nadeesha Dilruwan <eclayaz@gmail.com>
2020-02-27 18:34:49 +0530

    add middleware

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const members = require("./Memebers");
+const moment = require("moment");
 
 const app = express();
 
@@ -11,6 +12,18 @@ const app = express();
 
 // Set static folder
 app.use(express.static(path.join(__dirname, "public")));
+
+const logger = (req, res, next) => {
+  console.log(
+    `${req.protocol}://${req.get("host")}${
+      req.originalUrl
+    } : ${moment().format()}`
+  );
+  next();
+};
+
+// Init middleware
+app.use(logger);
 
 // Get all members
 app.get("/api/members", (req, res) => {
