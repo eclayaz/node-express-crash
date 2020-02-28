04d20d50e63be99e9fb9c60f92067e15d5ad1fc5
Nadeesha Dilruwan <eclayaz@gmail.com>
2020-02-28 17:14:41 +0530

    delete member

--- routes/api/members.js
+++ routes/api/members.js
@@ -54,4 +54,18 @@ router.put("/:id", (req, res) => {
   }
 });
 
+// Delete member
+router.delete("/:id", (req, res) => {
+  const found = members.some(member => member.id === parseInt(req.params.id));
+
+  if (found) {
+    res.json({
+      mag: "Member deleted",
+      members: members.filter(member => member.id !== parseInt(req.params.id))
+    });
+  } else {
+    res.status(400).json({ msg: `No member with the id of ${req.params.id}` });
+  }
+});
+
 module.exports = router;
